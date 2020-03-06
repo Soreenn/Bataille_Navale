dfd9ab56e3f67a45b839f8a2c667ebf0cd9c7ff6
Mad <35235150+Epsilonnnnn@users.noreply.github.com>
2020-03-06 14:54:43 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Bataille_Navale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Bataille_Navale.exe"
+  "Bataille_Navale.exe.manifest"
+  "Bataille_Navale.pdb"
+  "CMakeFiles/Bataille_Navale.dir/main.c.obj"
+  "libBataille_Navale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Bataille_Navale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
