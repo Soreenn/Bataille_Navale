9bae975612cd03963f53d53ac9861f74f59f691a
Mad <35235150+MadScientistee@users.noreply.github.com>
2020-03-13 10:39:25 +0100

    Création de la fonction "Quitter"

--- main.c
+++ main.c
@@ -3,16 +3,37 @@
 #include <windows.h>
 #pragma set_character_execution("UTF-8")
 
-int MenuPrincipal(){
-    int Choix;
-    printf("--- Bataille Navale --- \n");
-    printf("1 - Jouer \n");
-    printf("2 - Aide \n");
-    printf("3 - Quitter \n\n");
-    printf("Séléction : ");
-    scanf("%d", &Choix);
+int MenuPrincipal();
+
+void Aide() {
+    printf("--- Aide ---\n");
+    system("Pause");
+}
+
+int Quitter(){
+    printf("--- Quitter ---\n\n");
+    char Quit;
+    printf("Vous êtes sûr(e) de vouloir quitter? \n\n");
+    printf("Réponse (Y/N) : ");
+    scanf("%c", &Quit);
     fflush(stdin);
-    return Choix;
+    if(Quit == 89 || Quit == 121 ){
+
+    }
+    else{
+        if(Quit == 78 || Quit == 110){
+            MenuPrincipal();
+        }
+        else{
+            printf("\n\n Mauvaise réponse \n\n");
+            Quitter();
+        }
+    }
+}
+
+void InterfaceDeJeu(){
+    printf("--- Bataille Navale ---\n");
+    system("Pause");
 }
 
 void Redirection (Choix){
@@ -31,28 +52,22 @@ void Redirection (Choix){
     }
 }
 
-void Aide() {
-    printf("--- Aide ---\n");
-    system("Pause");
-}
-
-void Quitter(){
-    printf("--- Quitter ---\n");
-    system("Pause");
-}
-
-void InterfaceDeJeu(){
-    printf("--- Bataille Navale ---\n");
-    system("Pause");
+int MenuPrincipal(){
+    int Choix;
+    printf("--- Bataille Navale --- \n");
+    printf("1 - Jouer \n");
+    printf("2 - Aide \n");
+    printf("3 - Quitter \n\n");
+    printf("Selection : ");
+    scanf("%d", &Choix);
+    fflush(stdin);
+    Redirection(Choix);
 }
 
 int main() {
     SetConsoleOutputCP(65001);
 
-    int Choix = MenuPrincipal();
-
-    Redirection(Choix);
-
+    MenuPrincipal();
 
     return 0;
 }
